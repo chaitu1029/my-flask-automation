288b3e07d4f17b61adfd719c8dc5c120ac095ab2
Chaitanya Ram <chaitu.chaitu93@gmail.com>
2025-08-02 20:48:33 +0530

    Add Selenium automation route and threading in app.py

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-from flask import Flask, render_template, redirect, url_for
-import threading
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/run-python-code')
-def run_python_code():
+@app.route('/')
+def home():
     return render_template('index.html')
 
 def run_selenium_script():
